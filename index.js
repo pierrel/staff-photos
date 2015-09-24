60f2af8321f7c36c989bfb8d4b6eec3de33c4c23
Pierre Larochelle <plarochelle@blurb.com>
2015-09-24 04:06:26 +0000

    Adding basics of dropbox integration

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-var express = require('express');
-var app = express();
+const Dropbox = require('dropbox');
+const express = require('express');
+const app = express();
+
+// setup dropbox
+const dropbox_key = process.env.DROPBOX_KEY;
+const dropbox_secret = process.env.DROPBOX_SECRET;
+const dropbox_client = new Dropbox.Client({ 
+  key: dropbox_key,
+  secret: dropbox_secret
+});
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function() {
-  var addy = server.address();
-  var host = addy.address;
-  var port = addy.port;
+const server = app.listen(3000, function() {
+  const addy = server.address();
+  const host = addy.address;
+  const port = addy.port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
