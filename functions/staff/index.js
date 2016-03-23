ec80841808cfa7ead6604f3468a1e69b1bb9b0b5
Pierre Larochelle <plarochelle@blur.com>
2016-03-23 02:05:02 -0400

    Commenting-out some unused stuff and adding return

--- functions/staff/index.js
+++ functions/staff/index.js
@@ -1,18 +1,15 @@
 const Dropbox = require('dropbox');
-const express = require('express');
 const html = require('./html');
 const view = require('./view');
 
-const app = express();
-
-const dropbox_key = process.env.DROPBOX_KEY;
-const dropbox_secret = process.env.DROPBOX_SECRET;
-const dropbox_token = process.env.DROPBOX_APP_KEY;
-const dropbox_client = new Dropbox.Client({ 
-  key: dropbox_key,
-  secret: dropbox_secret,
-  token: dropbox_token
-});
+//const dropbox_key = process.env.DROPBOX_KEY;
+//const dropbox_secret = process.env.DROPBOX_SECRET;
+//const dropbox_token = process.env.DROPBOX_APP_KEY;
+//const dropbox_client = new Dropbox.Client({ 
+//  key: dropbox_key,
+//  secret: dropbox_secret,
+//  token: dropbox_token
+//});
 
 const PHOTO_DIR = '/Photos/Sample Album'; 
 
@@ -32,25 +29,19 @@ function showThumbs(res, entries) {
   res.send(view.layout(thumbs));
 }
 
+exports.handle = function(e, ctx) {
+  ctx.succeed({hello: "World"});
+}
 
-app.get('/', function(req, res) {
-  dropbox_client.readdir(PHOTO_DIR, function(error, entries) {
-    if (error) {
-      res.send('there was an error');
-      console.log(error);
-    } else {
-      const fullEntries = entries.map(function(entry) { return PHOTO_DIR + '/' + entry; });
-      showThumbs(res, fullEntries);
-    }
-  });
-});
 
-app.use('/public', express.static('public'));
-
-const server = app.listen(3000, function() {
-  const addy = server.address();
-  const host = addy.address;
-  const port = addy.port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+//app.get('/', function(req, res) {
+//  dropbox_client.readdir(PHOTO_DIR, function(error, entries) {
+//    if (error) {
+//      res.send('there was an error');
+//      console.log(error);
+//    } else {
+//      const fullEntries = entries.map(function(entry) { return PHOTO_DIR + '/' + entry; });
+//      showThumbs(res, fullEntries);
+//    }
+//  });
+//});
